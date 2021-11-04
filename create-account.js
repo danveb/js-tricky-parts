027d7a7c343c22ad523edde887fa3e96789cdeb2
Daniel Bae <danibae91@icloud.com>
2021-11-04 16:42:42 -0700

    submit finished code

--- create-account.js
+++ create-account.js
@@ -1,5 +1,49 @@
-function createAccount(pin, amount) {
-
+const createAccount = (pin, amount=0) => {
+    return {
+        checkBalance: function(password) {
+            if(password !== pin) {
+                return 'Invalid PIN.'
+            } 
+            return `$${amount}`
+        }, 
+        deposit: function(password, newAmount) {
+            if(password !== pin) {
+                return 'Invalid PIN.'
+            }
+            // make the deposit to the account
+            amount += newAmount 
+            return `Successfully deposited $${newAmount}. Current balance: $${amount}.`
+        },
+        withdraw: function(password, withdrawAmount) {
+            if(password !== pin) {
+                return 'Invalid PIN.'
+            }
+            if(withdrawAmount > amount) {
+                return 'Withdrawal amount exceeds account balance. Transaction cancelled.'
+            }
+            // make the widthdrawal from the account
+            amount -= withdrawAmount 
+            return `Successfully withdrew $${withdrawAmount}. Current balance: $${amount}.`
+        }, 
+        changePin: function(oldPin, newPin) {
+            if(oldPin !== pin) {
+                return 'Invalid PIN.'
+            } 
+            pin = newPin 
+            return 'PIN successfully changed!'
+        }
+    }
 }
+
+// let account = createAccount('1234', 100); 
+// console.log(account)
+// console.log(account.checkBalance('oops'))
+// console.log(account.checkBalance('1234'))
+// console.log(account.deposit('1234', 100))
+// console.log(account.widthdraw('1234', 200))
+// console.log(account.changePin('1233', '0000'))
+// console.log(account.changePin('1234', '0000'))
+// console.log(account.checkBalance('1234'))
+// console.log(account.checkBalance('0000'))
 
 module.exports = { createAccount };
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,33 @@
-function guessingGame() {
+const guessingGame = () => {
+    let target = Math.floor(Math.random() * 100) 
+    let guessCount = 0; 
+    let gameOver = false; 
 
+    return function game(num) {
+        // if game is not over we have to continue checking
+        if(gameOver) {
+            guessCount++
+            return 'The game is over, you already won!'
+        }
+
+        if(num === target) {
+            guessCount++
+            gameOver = true 
+            let guess = guessCount === 1 ? 'guess' : 'guesses'
+            return `You win! You found ${num} in ${guessCount} ${guess}.`
+        }
+        if(num < target) {
+            guessCount++
+            return `${num} is too low!`
+        } 
+        if(num > target) {
+            guessCount++
+            return `${num} is too high!`
+        }  
+    }
 }
+// let game = guessingGame() 
+// console.log(game(50))
+// console.log(game(71))
 
 module.exports = { guessingGame };
